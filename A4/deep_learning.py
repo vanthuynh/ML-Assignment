a0adab8eab1ed6ff6d34b0aabebbce8afb65247d
vanthuynh <vthuynh@cpp.edu>
2023-04-22 23:27:38 -0700

    update deep_learning.py

--- A4/deep_learning.py
+++ A4/deep_learning.py
@@ -14,27 +14,29 @@ from tensorflow import keras
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.metrics import accuracy_score
 
 def build_model(n_hidden, n_neurons_hidden, n_neurons_output, learning_rate):
 
     #-->add your Pyhton code here
 
     #Creating the Neural Network using the Sequential API
-    #model = keras.models.Sequential()
-    #model.add(keras.layers.Flatten(input_shape=[28, 28]))                                #input layer
+    model = keras.models.Sequential()
+    model.add(keras.layers.Flatten(input_shape=[28, 28]))                                #input layer
 
     #iterate over the number of hidden layers to create the hidden layers:
-    #model.add(keras.layers.Dense(n_neurons_hidden, activation="relu"))                   #hidden layer with ReLU activation function
+    for x in range(n_hidden):
+        model.add(keras.layers.Dense(n_neurons_hidden, activation="relu"))                   #hidden layer with ReLU activation function
 
     #output layer
-    #model.add(keras.layers.Dense(n_neurons_output, activation="softmax"))                #output layer with one neural for each class and the softmax activation function since the classes are exclusive
+    model.add(keras.layers.Dense(n_neurons_output, activation="softmax"))                #output layer with one neural for each class and the softmax activation function since the classes are exclusive
 
     #defining the learning rate
-    #opt = keras.optimizers.SGD(learning_rate)
+    opt = keras.optimizers.SGD(learning_rate)
 
     #Compiling the Model specifying the loss function and the optimizer to use.
-    #model.compile(loss="sparse_categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
-    #return model
+    model.compile(loss="sparse_categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
+    return model
 
 
 #To install Tensor Flow on your terminal
@@ -59,27 +61,34 @@ n_hidden = [2, 5, 10]
 n_neurons = [10, 50, 100]
 l_rate = [0.01, 0.05, 0.1]
 
-for :                          #looking or the best parameters w.r.t the number of hidden layers
-    for :                      #looking or the best parameters w.r.t the number of neurons
-        for :                  #looking or the best parameters w.r.t the learning rate
+highestAccuracy = 0
+for h in n_hidden:                          #looking or the best parameters w.r.t the number of hidden layers
+    for n in n_neurons:                      #looking or the best parameters w.r.t the number of neurons
+        for l in l_rate:                  #looking or the best parameters w.r.t the learning rate
 
             #build the model for each combination by calling the function:
             #model = build_model()
             #-->add your Pyhton code here
+            model = build_model(h,n,10,l)
 
             #To train the model
-            #history = model.fit(X_train, y_train, epochs=5, validation_data=(X_valid, y_valid))  #epochs = number times that the learning algorithm will work through the entire training dataset.
-            #-->add your Pyhton code here
+            history = model.fit(X_train, y_train, epochs=5, validation_data=(X_valid, y_valid))  #epochs = number times that the learning algorithm will work through the entire training dataset.
 
             #Calculate the accuracy of this neural network and store its value if it is the highest so far. To make a prediction, do:
             class_predicted = np.argmax(model.predict(X_test), axis=-1)
             #-->add your Pyhton code here
+            accuracy = accuracy_score(y_test, class_predicted)
+            if(accuracy > highestAccuracy):
+                highestAccuracy = accuracy
 
             print("Highest accuracy so far: " + str(highestAccuracy))
             print("Parameters: " + "Number of Hidden Layers: " + str(h) + ",number of neurons: " + str(n) + ",learning rate: " + str(l))
             print()
 
 #After generating all neural networks, print the summary of the best model found
+weights, biases = model.layers[1].get_weights()
+print(weights)
+print(biases)
 #The model’s summary() method displays all the model’s layers, including each layer’s name (which is automatically generated unless you set it when creating the layer), its
 #output shape (None means the batch size can be anything), and its number of parameters. Note that Dense layers often have a lot of parameters. This gives the model quite a lot of
 #flexibility to fit the training data, but it also means that the model runs the risk of overfitting, especially when you do not have a lot of training data.
